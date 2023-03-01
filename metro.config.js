7088a7f4a574457822173d7501f48098a96e7633
Ashok Amaran <ashok@ashokamaran.com>
2023-03-01 14:04:28 -0500

    Added svg icons

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,23 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = getDefaultConfig(__dirname);
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
---
+++ src/assets/images/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as TicketIcon } from './ic-ticket.svg';
+export { default as BagIcon } from './ic-bag.svg';
+export { default as UserIcon } from './ic-user.svg';
